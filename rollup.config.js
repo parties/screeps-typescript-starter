55cbe361a2152c2e4b6401dd48c88419f3f87843
Eric Ogden <parties@users.noreply.github.com>
2024-12-13 20:32:44 +0000

    trying to get auth token from github

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import screeps from 'rollup-plugin-screeps';
+import replace from 'rollup-plugin-replace';
 
 let cfg;
 const dest = process.env.DEST;
@@ -12,6 +13,11 @@ if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
 } else if ((cfg = require("./screeps.json")[dest]) == null) {
   throw new Error("Invalid upload destination");
+}
+
+const token = process.env.SCREEPS_TOKEN;
+if (!token) {
+  throw new Error('Invalid screeps token');
 }
 
 export default {
@@ -23,10 +29,14 @@ export default {
   },
 
   plugins: [
+    replace({
+      preventAssignment: true,
+      'process.env.SCREEPS_TOKEN': JSON.stringify(process.env.SCREEPS_TOKEN || ''),
+    }),
     clear({ targets: ["dist"] }),
     resolve({ rootDir: "src" }),
     commonjs(),
     typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null})
+    screeps({config: cfg, dryRun: cfg == null}),
   ]
 }
